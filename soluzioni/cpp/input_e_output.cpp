3e1dd046c6477f66b82fe3b26f84114a30cca48d
rZwols <don.como.dino.sauro@gmail.com>
2025-12-19 16:37:17 +0100

    first files and directories

---
+++ soluzioni/cpp/input_e_output.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+
+    cout << "Hello world!" << "\n";
+
+    cout << "Nome e Cognome \n";
+
+    cout << "Nome \n";
+    cout << "Cognome \n";
+
+    int numero;
+    cout << "inserire un numero: ";
+    cin >> numero;
+
+    int a, b, c;
+    cout << "inserire 3 numeri: ";
+    cin >> a >> b >> c;
+
+    cout << c << " - " << b << " - " << a << "\n";
+
+    return 0;
+}
---
+++ soluzioni/python/input_e_output.py
@@ -0,0 +1,14 @@
+print("Hello world!")
+
+print("nome e cognome")
+
+print("nome")
+print("cognome")
+
+numero = input("inserire un numero: ")
+
+a = input("inserire primo numero: ")
+b = input("inserire secondo numero: ")
+c = input("inserire terzo numero: ")
+
+print(a + " - " + b + " - " + c)
